cc7df5b567f44f6d5c149d22cf9cf092f0913a5f
nshush <kagekao137@gmail.com>
2024-11-28 21:00:10 +0300

    Added task 1

---
+++ Workshop/Task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace MiniCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите первое число: ");
+                    double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+                    Console.Write("Введите второе число: ");
+                    double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+                    double result = AddNumbers(firstNumber, secondNumber);
+
+                    Console.WriteLine($"Сумма {firstNumber} и {secondNumber} равна {result}.");
+                    break; 
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка: Пожалуйста, введите корректное число.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Произошла ошибка: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("Вычисление завершено.");
+                }
+            }
+        }
+
+        static double AddNumbers(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
